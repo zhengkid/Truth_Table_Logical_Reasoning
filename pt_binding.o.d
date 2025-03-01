7ad4ffc6d0941cfd6ff08999376a890e0c4110ec
Tong Zheng <tzheng24@umd.edu>
2025-03-01 11:15:51 -0500

    add exec check

--- pt_binding.o.d
+++
@@ -1,6 +0,0 @@
-pt_binding.o: \
- /beacon-scratch/tongzh24/miniconda3/envs/logical_reasoning/lib/python3.11/site-packages/deepspeed/ops/csrc/transformer/inference/csrc/pt_binding.cpp \
- /beacon-scratch/tongzh24/miniconda3/envs/logical_reasoning/lib/python3.11/site-packages/deepspeed/ops/csrc/transformer/inference/includes/inference_context.h \
- /beacon-scratch/tongzh24/miniconda3/envs/logical_reasoning/lib/python3.11/site-packages/deepspeed/ops/csrc/transformer/inference/includes/inference_cublas_wrappers.h \
- /beacon-scratch/tongzh24/miniconda3/envs/logical_reasoning/lib/python3.11/site-packages/deepspeed/ops/csrc/transformer/inference/includes/inference_cuda_layers.h \
- /beacon-scratch/tongzh24/miniconda3/envs/logical_reasoning/lib/python3.11/site-packages/deepspeed/ops/csrc/includes/ds_kernel_utils.h
